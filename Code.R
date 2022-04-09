75d3358cabd2fe53d59bc1a9d2e597f865e32182
Jhonattan Lino <jhonattanlino@outlook.com>
2022-04-09 17:01:39 -0300

    Update Code.R

--- Code.R
+++ Code.R
@@ -3,8 +3,10 @@ library(tidyverse)
 library(tidyquant)
 library(ggplot2)
 
-sapr <- tidyquant::tq_get('SAPR3.SA', get = 'stock.prices', from = '2020-01-01', to = Sys.Date())
+### Importando dados
+sapr <- tidyquant::tq_get('SAPR4.SA', get = 'stock.prices', from = '2020-01-01', to = Sys.Date()) ## dados referente a sanepar
 
+### Criando gráfico de preços históricos
 sapr %>%
   ggplot(aes(x=date, y=adjusted))+
   geom_line()+
@@ -12,13 +14,22 @@ sapr %>%
     x='Data',
     y='SAPR3',
     title='SANEPAR',
-    subtitle = 'Preços ajustados - 2020-01-01 - '
+    subtitle = 'Preços ajustados - 2020-01-01 - Hoje'
   )
 
+### Criando séries de retornos
 sapr_pct <- sapr %>%
   arrange(date) %>%
-  mutate(ret = (adjusted - lag(adjusted)) / lag(adjusted)) %>%
-  drop_na(ret) %>%
+  mutate(ret = (adjusted - lag(adjusted)) / lag(adjusted)) %>% ## criando a difença entre os dias
+  drop_na(ret) %>% ## excluindo os valores nulos
   select(symbol, date, ret)
+
+### Plotando gráfico dos retornos
+sapr_pct %>%
+  ggplot(aes(x=ret*100))+
+  geom_histogram(bins = 80)+
+  labs(
+    x=NULL,
+    y=NULL)
 
   
